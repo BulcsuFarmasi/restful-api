d51c38fcdf22926cb69f407697552cbd47be269c
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-01-17 15:10:50 +0100

    deleteing an order

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -8,7 +8,7 @@ const Product = require('../models/product');
 // Handle incoming GET requests to /orders
 router.get('/', (req, res, next) => {
     Order.find()
-        .select('prooduct quantity _id')
+        .select('product quantity _id')
         .exec()
         .then(docs => {
             res.status(200).json({
@@ -75,6 +75,11 @@ router.get('/:orderId', (req, res, next) => {
     Order.findById(req.params.orderId)
         .exec()
         .then(order => {
+            if (!order) {
+                return res.status(404).json({
+                    message: 'Order not found'
+                })
+            }
             res.status(200).json({
                 order: order,
                 request: {
@@ -91,10 +96,23 @@ router.get('/:orderId', (req, res, next) => {
 });
 
 router.delete('/:orderId', (req, res, next) => {
-    res.status(201).json({
-        message: 'Order deleted',
-        orderId: req.params.orderId
-    })
+    Order.remove({ _id: req.params.orderId })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Order deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/orders',
+                    body: {productId: 'ID', quantity: 'Number'}
+                }
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
 });
 
 module.exports = router;
