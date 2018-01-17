155db58f4c844ef5acca12137bb24281213ac3e3
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-01-17 14:19:11 +0100

    fetching orders

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -6,9 +6,30 @@ const Order = require('../models/order');
 
 // Handle incoming GET requests to /orders
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Orders was fetched!'
-    })
+    Order.find()
+        .select('prooduct quantity _id')
+        .exec()
+        .then(docs => {
+            res.status(200).json({
+                count: docs.length,
+                orders: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/orders/' + doc._id
+                        }
+                    }
+                })
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
 });
 
 router.post('/', (req, res, next) => {
