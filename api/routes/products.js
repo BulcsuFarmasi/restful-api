ea9ed5fd5ba41473d1acdcfdb00771bc883e2ad9
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-01-22 23:29:41 +0100

    create update product method

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router =  express.Router();
-const mongoose = require('mongoose');
 const multer = require('multer');
 const checkAuth = require('../middleware/check-auth');
 const ProductsController =require('../controllers/products');
@@ -37,24 +36,6 @@ router.get('/:productId', ProductsController.products_get_product);
 
 router.patch('/:productId', checkAuth, ProductsController.products_update_product);
 
-router.delete('/:productId', checkAuth, (req, res, next) => {
-    const id = req.params.productId;
-    Product.remove({_id: id})
-        .exec()
-        .then(result => {
-            res.status(200).json({
-                message: 'Product deleted',
-                request: {
-                    type: 'POST',
-                    url: 'http:/localhost:3000/products',
-                    body: {name: 'String' , price: 'Number'}
-                }
-            });
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json({error: err});
-        });
-}
+router.delete('/:productId', checkAuth, ProductsController.products_delete_product);
 
 module.exports = router;
