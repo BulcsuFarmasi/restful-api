87748892700adadb777393f57bf75c7ae00508cc
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2018-01-22 23:40:02 +0100

    create login  method

--- api/routes/users.js
+++ api/routes/users.js
@@ -9,48 +9,7 @@ const UsersControler = require('../controllers/users');
 
 router.post('/signup', UsersControler.users_signup);
 
-router.post('/login', (req, res, next) => {
-    User.find({email: req.body.email})
-        .exec()
-        .then(user => {
-            if (user.length < 1) {
-                return res.status(401).json({
-                    message: 'Auth failed'
-                })
-            }
-            bcrypt.compare(req.body.password, user[0].password, (err, result) => {
-                if (err) {
-                    return res.status(401).json({
-                        message: 'Auth failed'
-                    })
-                }
-                if (result) {
-                    const token = jwt.sign({
-                        email: user[0].email,
-                        userId: user[0]._id
-                    },
-                    process.env.JWT_KEY,
-                    {
-                        expiresIn:  '1h'
-                    },
-
-                    );
-                    return res.status(200).json({
-                        message: 'Auth successful',
-                        token: token
-                    });
-                }
-                return res.status(401).json({
-                    message: 'Auth failed',
-                })
-            })
-        })
-        .catch(err => {
-            res.status(500).json({
-                error: err
-            })
-        });
-});
+router.post('/login', UsersControler.users_login);
 
 router.delete('/:userId', (req, res, next) => {
     User.remove({_id: req.params.userId})
